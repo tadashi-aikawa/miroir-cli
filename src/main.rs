05ac4c7bdcdd7e0d0325e40fe5ac906126f4536b
tadashi-aikawa <syou.maman@gmail.com>
2018-08-02 09:21:37 +0900

    WIP: Fetch summaries #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+extern crate rusoto_core;
+extern crate rusoto_dynamodb;
+
+use rusoto_core::Region;
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ScanInput};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Summary {
+    hashkey: String,
+    title: Option<String>,
+    // Option is for old version
+//    elapsed_sec: Option<String>,
+}
+
+fn fetch_summaries(client: &DynamoDbClient, table_name: String) -> Vec<Summary> {
+    let scan_input = ScanInput {
+        table_name,
+        ..Default::default()
+    };
+
+    match client.scan(&scan_input).sync() {
+        Ok(output) => {
+            output.items.unwrap().into_iter()
+                .map(|x| {
+                    Summary {
+                       hashkey: x.get("hashkey").cloned().unwrap().s.unwrap(),
+                        title: x.get("title").cloned().unwrap().s,
+//                        elapsed_sec: x.get("elapsed_sec").cloned().unwrap().n,
+                    }
+                })
+                .collect::<Vec<Summary>>()
+        }
+        Err(error) => {
+            println!("Error: {:?}", error);
+            panic!(error)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let client = DynamoDbClient::simple(Region::ApNortheast1);
+    let summaries = fetch_summaries(&client, "miroir".to_string());
+    let json_str = serde_json::to_string(&summaries).unwrap();
+    print!("{}", json_str);
 }
